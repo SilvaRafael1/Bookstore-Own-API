af79476aa28e59dbf533f37cddaf9821515721ce
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2023-11-28 15:34:09 -0300

    NPM config

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+const cors = require("cors")
+
+const app = express();
+const uri = "mongodb://127.0.0.1:27017";
+
+mongoose.connect(uri, { dbName: "db_Prova" });
+// requireDir("./models");
+
+app.use(cors())
+app.use(express.json())
+// app.use("/api", require("./routes"));
+
+app.listen(3000, () => {
+  console.log("Exemplo de aplicativo ouvindo a porta 3000");
+});
